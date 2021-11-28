a074a7e72d8603fca9385d8a7adea99f2ebe39da
luthfy <luthfy.megumi@gmail.com>
2021-11-28 22:45:02 +0800

    install tailwind and setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const production = !process.env.ROLLUP_WATCH;
+module.exports = {
+	future: {
+		purgeLayersByDefault: true,
+		removeDeprecatedGapUtilities: true,
+	},
+	plugins: [],
+	purge: {
+		content: [
+			'./src/App.svelte'
+		],
+		enabled: production
+	}
+};
